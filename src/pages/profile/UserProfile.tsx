975d91b2f2a00015cdb8c6d82d1c4e27034c42ef
Vishal Kumar Sahu <vishal.sahu@indiconnect.in>
2024-07-13 23:18:56 +0530

    Save user location to firebase

--- src/pages/profile/UserProfile.tsx
+++ src/pages/profile/UserProfile.tsx
@@ -1,25 +1,37 @@
 import React, {useEffect, useState} from "react";
 import {AppBar, Box, Button, IconButton, Toolbar, Typography} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
-import {getCurrentUser, logoutUser, onAuthChange} from "../../services/authService";
+import {getCurrentUser, logoutUser, logUserLocation, onAuthChange} from "../../services/authService";
 import {useNavigate} from "react-router-dom";
 import {useSnackbar} from "../../components/SnackbarContext";
 
 const UserProfilePage = () => {
 
   const [user, setUser] = useState<any>(null);
+  const [locationInterval, setLocationInterval] = useState<NodeJS.Timeout | null>(null);
   const navigate = useNavigate();
   const {showMessage} = useSnackbar();
 
   useEffect(() => {
     const currentUser = getCurrentUser();
     if (currentUser) {
-      console.log('User', currentUser);
       setUser(currentUser);
+      const interval = setInterval(() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            logUserLocation(currentUser.uid, {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+          },
+          (error) => {
+            showMessage(`Error getting location: ${error}`, 'error')
+          });
+      }, 5000);
+      setLocationInterval(interval);
     } else {
       const unsubscribe = onAuthChange((user) => {
         if (user) {
-          console.log('Unsubscribe', user);
           setUser(user);
         } else {
           navigate('/');
